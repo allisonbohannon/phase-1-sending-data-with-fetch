2bf69a3b51ba4216df4e2e27910a932478ba71b7
Allison Bohannon <akboh.github@gmail.com>
2022-05-07 18:19:43 -0700

    commit tree a44d56189128171dbf7222de242fbce479ac1964

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Add your code here
+
+function submitData(name, email) {
+    fetch('http://localhost:3000/users', {
+        method: 'POST', 
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+          },
+        body: JSON.stringify({
+            name: name, 
+            email: email
+        }), 
+    })
+    .then(function (response) {
+        return response.json();
+      })
+      .then(function (object) {
+        const newUser = document.createElement('p');
+        const newEmail = document.createElement('p')
+        const newId = document.createElement('p')
+        newUser.textContent = object.name; 
+        newEmail.textContent = object.email; 
+        newId.textContent = object.id; 
+        document.querySelector('body').appendChild(newUser).appendChild(newEmail).appendChild(newId);
+
+      })
+      .catch(function (error) {
+        const err = document.createElement('h1'); 
+        err.textContent = error.message; 
+        document.querySelector('body').appendChild(err);
+      })
+}; 
+
+submitData('Allison', 'Allison@email.com')
